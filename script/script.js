6567c181067295fc99cba9a150df6e92c495e573
Dilmurod001 <dilmurodjadanov0109@gmail.com>
2021-09-08 15:32:29 +0500

    update

---
+++ script/script.js
@@ -0,0 +1,4 @@
+function navbar(){
+  const bar= document.getElementById("bricon").innerHTML="<div> <button>x</button><br> <br><ul><li>About</li> <li>History</li>  <li>Contact</li> </ul> </div>"
+    document.getElementById("bricon").style="margin-bottom: 5px; bacground: white"
+}
